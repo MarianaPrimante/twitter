e8ec48d136bfa1489f5f23528a77cb897d6c3195
MarianaPrimante <maprimante@gmail.com>
2019-02-19 09:40:51 -0300

    versão 003

--- src/twitter.js
+++ src/twitter.js
@@ -1,5 +1,4 @@
-function twittar(event) {
-    var evento = window.event || event;
+function twittar() {
     let tweet = document.getElementById("twitter").value;
     document.getElementById("posttwitter").innerHTML = tweet
 }
@@ -12,20 +11,24 @@ function contarCaracteres() {
     let max = 140;
     let atual = document.getElementById('twitter').value.length;
     let valor = max - atual;
-    if (valor > 0 && valor < 140) {
+    if (valor > 0) {
         if (valor === 1) { valor = 0; }
         document.getElementById("restante").innerHTML = valor;
         document.getElementById("btntwittar").disabled = "";
+        document.getElementById("restante").style.color = "black";
     }
     if (valor === 140) {
         document.getElementById("btntwittar").disabled = "disabled";
     }
-    if (max > 120) {
+    if (valor <= 20) {
+        document.getElementById("restante").style.color = "green";
+    }
+    if (valor <= 10) {
         document.getElementById("restante").style.color = "yellow";
     }
-    if (max > 130) {
-        document.getElementById("restante").style.color = "red";
-    } else {
+    if (valor < 0) {
         document.getElementById("btntwittar").disabled = "disabled";
+        document.getElementById("restante").innerHTML = valor;
+        document.getElementById("restante").style.color = "red";
     }
 }
