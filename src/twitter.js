3926bff724e37c2d1c287e5d26cb58938dc589fb
Mariana <maprimante@gmail.com>
2019-06-21 13:33:49 -0300

    refactoring

--- src/twitter.js
+++ src/twitter.js
@@ -1,68 +1,77 @@
 function twittar() {
-    let tweetOne = document.createElement("p");
-    let tweetTwo = document.createElement("p");
-    let li = document.createElement("li");
-    let tweet = document.getElementById("twitter").value;
-    tweetOne.innerHTML = tweet;
-    tweetTwo.innerHTML = time();
-    li.appendChild(tweetOne);
-    li.appendChild(tweetTwo);
-    document.getElementById("posttotal").appendChild(li);
-    document.getElementsByTagName('form')[0].reset()
+  const tweetOne = document.createElement("p");
+  const tweetTwo = document.createElement("p");
+  const line = document.createElement("li");
+  const tweet = document.getElementById("twitter").value;
+  tweetOne.innerHTML = tweet;
+  tweetTwo.innerHTML = time();
+  line.appendChild(tweetOne);
+  line.appendChild(tweetTwo);
+  document.getElementById("post-total").appendChild(line);
+  document.getElementsByTagName("form")[0].reset();
+  countCharacters();
+  getKey(event);
 }
 
-document.getElementById('btntwittar').addEventListener('click', twittar);
+document.getElementById("btntwittar").addEventListener("click", twittar);
 
-function contarCaracteres() {
-    let max = 140;
-    let atual = document.getElementById('twitter').value.length;
-    let valor = max - atual;
-    if (valor > 0) {
-        if (valor === 1) { valor = 0; }
-        document.getElementById("restante").innerHTML = valor;
-        document.getElementById("btntwittar").disabled = "";
-        document.getElementById("restante").style.color = "black";
-    }
-    if (valor === 140) {
-        document.getElementById("btntwittar").disabled = "disabled";
-    }
-    if (valor <= 20) {
-        document.getElementById("restante").style.color = "#1c94e0";
-    }
-    if (valor <= 10) {
-        document.getElementById("restante").style.color = "#a3b0bd";
-    }
-    if (valor < 0) {
-        document.getElementById("btntwittar").disabled = "disabled";
-        document.getElementById("restante").innerHTML = valor;
-        document.getElementById("restante").style.color = "red";
-    }
+function countCharacters() {
+  const max = 140;
+  let current = document.getElementById("twitter").value.length;
+  let amount = max - current;
+  let btnTwittar = document.getElementById("btntwittar");
+  let rest = document.getElementById("rest");
+  if (amount > 0) {
+    if (amount === 1) { amount = 0; }
+    rest.innerHTML = amount;
+    btnTwittar.disabled = "";
+    rest.style.color = "black";
+  }
+  if (amount === 140) {
+    btnTwittar.disabled = "disabled";
+  }
+  if (amount <= 20) {
+    rest.style.color = "#1c94e0";
+  }
+  if (amount <= 10) {
+    rest.style.color = "#a3b0bd";
+  }
+  if (amount < 0) {
+    btnTwittar.disabled = "disabled";
+    rest.innerHTML = amount;
+    rest.style.color = "red";
+  }
 }
 
-
-let lengthPostTwitter = document.getElementById("twitter");
-lengthPostTwitter.addEventListener('keydown', getKey);
+const lengthPostTwitter = document.querySelector("#twitter");
+lengthPostTwitter.addEventListener("keydown", getKey);
+lengthPostTwitter.addEventListener("click", countCharacters);
+lengthPostTwitter.addEventListener("keyup", countCharacters);
 
 function getKey(event) {
-    let code = event.keyCode;
-    if (lengthPostTwitter.scrollHeight > lengthPostTwitter.offsetHeight || code === 13 && code !== 8) {
-        lengthPostTwitter.rows += 1;
-    }
-    if (lengthPostTwitter.scrollHeight < lengthPostTwitter.offsetHeight && code === 8) {
-        lengthPostTwitter.rows -= 1;
-    }
+  const code = event.keyCode;
+  if (lengthPostTwitter.scrollHeight > lengthPostTwitter.offsetHeight || code === 13) {
+    lengthPostTwitter.rows += 1;
+  } 
+  if (lengthPostTwitter.scrollHeight === lengthPostTwitter.offsetHeight && code === 8) {
+    if (lengthPostTwitter.rows === 1) {
+      lengthPostTwitter.rows = 1;
+    } else {
+      lengthPostTwitter.rows -= 1;
+    } 
+  }
 }
 
 function time() {
-    let today = new Date();
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let strMin = min.toString();
-    let strHour = hour.toString();
-    if (strHour.length < 2)
-        strHour = "0" + strHour;
-    if (strMin.length < 2)
-        strMin = "0" + strMin;
-    let timeToday = strHour + ":" + strMin;
-    return timeToday
+  let today = new Date();
+  let hour = today.getHours();
+  let min = today.getMinutes();
+  let strMin = min.toString();
+  let strHour = hour.toString();
+  if (strHour.length < 2)
+    strHour = "0" + strHour;
+  if (strMin.length < 2)
+    strMin = "0" + strMin;
+  let timeToday = strHour + ":" + strMin;
+  return timeToday;
 }
